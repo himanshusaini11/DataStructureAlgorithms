58feaacd739209cdce22db5a5c021568e8b36f8a
Himanshu Saini <68609811+himanshusaini11@users.noreply.github.com>
2024-07-16 17:55:02 -0400

    Update and rename 00-Max Consecutive Ones to 00-MaxConsecutiveOnes.py

--- 00-MaxConsecutiveOnes.py
+++ 00-MaxConsecutiveOnes.py
@@ -1,17 +1,16 @@
-## 1. Max Consecutive Ones
+##Max Consecutive Ones
 ### Example :
-```
+"""
 Input: nums = [1,1,0,1,1,1]
 Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
-```
-Constraints:
-```
+"""
+#Constraints:
+"""
 1 <= nums.length <= 105
 nums[i] is either 0 or 1.
-```
-
-~~~~python
+"""
+from typing import List
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         s = 0
@@ -20,4 +19,7 @@ class Solution:
             if i == 0:
                 s = 0
         return s
-~~~~
+if __name__ == "__main__":
+    solution = Solution()
+    test_case = [1, 1, 0, 1, 1, 1]
+    print(solution.findMaxConsecutiveOnes(test_case))  # Expected output: 3
