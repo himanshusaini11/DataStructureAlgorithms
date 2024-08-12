cffe449561566a6017f90fec588c11cb8304a321
Himanshu Saini <sainih5@outlook.com>
2024-08-12 13:28:33 -0400

    Linked List

---
+++ 03-LinkedList/00-MiddleOfLinkedList.py
@@ -0,0 +1,46 @@
+from typing import Optional
+import time
+
+start_time = time.time()
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
+
+def create_linked_list(values):
+    if not values:
+        return None
+    head = ListNode(values[0])
+    current = head
+    for value in values[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+def print_linked_list(head):
+    result = []
+    current = head
+    while current:
+        result.append(current.val)
+        current = current.next
+    return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    head = create_linked_list([1, 2, 3, 4, 5, 6])
+    middle = solution.middleNode(head)
+    print(print_linked_list(middle))
+    print("The time of execution of the above program is:", (time.time() - start_time) * 10**3, "ms")
