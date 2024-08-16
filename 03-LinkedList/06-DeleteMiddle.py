4878bee0eda5f94477c04148db78e8cd65275424
Himanshu Saini <sainih5@outlook.com>
2024-08-15 21:08:20 -0400

    Delete Middle Node

---
+++ 03-LinkedList/06-DeleteMiddle.py
@@ -0,0 +1,54 @@
+from typing import Optional
+import time
+
+start_time = time.time()
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+
+        slow = head
+        fast = head
+        temp = None
+
+        while fast and fast.next:
+            temp = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        temp.next = slow.next
+
+        return head
+            
+
+def create_linked_list(values):
+    if not values:
+        return None
+    head = ListNode(values[0])
+    current = head
+    for value in values[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+def print_linked_list(head):
+    result = []
+    current = head
+    while current:
+        result.append(current.val)
+        current = current.next
+    return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    head = create_linked_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    temp = solution.deleteMiddle(head)
+    print (print_linked_list(temp))
+    print("The time of execution of the above program is:", (time.time() - start_time) * 10**3, "ms")
